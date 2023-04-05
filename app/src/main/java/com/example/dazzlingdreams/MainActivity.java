11bb479b4fb9f7fbe8743e01c49ee521c3b79f29
Praful Deshmukh <prafuldeshmukh068@gmail.com>
2023-04-05 07:59:25 +0530

    CHANGES IN CONTACT USXML

--- app/src/main/java/com/example/dazzlingdreams/MainActivity.java
+++ app/src/main/java/com/example/dazzlingdreams/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.dazzlingdreams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.denzcoskun.imageslider.ImageSlider;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageSlider imageSlider;
+    LinearLayout AddMember,ShowMember,AddAttendence,ContactUs;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -16,6 +20,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView( R.layout.activity_main );
 
         imageSlider = findViewById(R.id.image_slider);
+
+        AddMember  = findViewById(R.id.add_member);
+        AddMember.setOnClickListener( new View.OnClickListener( ) {
+            @Override
+            public void onClick (View v) {
+                startActivity( new Intent( MainActivity.this, com.example.dazzlingdreams.AddMember.class ) );
+            }
+        } );
+
+
+        ShowMember  = findViewById(R.id.show_members);
+        ShowMember.setOnClickListener( new View.OnClickListener( ) {
+            @Override
+            public void onClick (View v) {
+                startActivity( new Intent( MainActivity.this,ShowMembers.class) );
+            }
+        } );
+
+
+        AddAttendence  = findViewById(R.id.add_attendence);
+        AddMember.setOnClickListener( new View.OnClickListener( ) {
+            @Override
+            public void onClick (View v) {
+                startActivity( new Intent( MainActivity.this, com.example.dazzlingdreams.AddMember.class ) );
+            }
+        } );
+
+        ContactUs  = findViewById(R.id.contact_us);
+        AddMember.setOnClickListener( new View.OnClickListener( ) {
+            @Override
+            public void onClick (View v) {
+                startActivity( new Intent( MainActivity.this, com.example.dazzlingdreams.AddMember.class ) );
+            }
+        } );
+
+
 
     }
 }
---
+++ app/src/main/java/com/example/dazzlingdreams/ShowMembers.java
@@ -0,0 +1,14 @@
+package com.example.dazzlingdreams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowMembers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_show_members );
+    }
+}
