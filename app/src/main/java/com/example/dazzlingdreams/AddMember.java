ab95910ce49624dd7837e6dadae9e0cc93e09779
Praful Deshmukh <prafuldeshmukh068@gmail.com>
2023-04-05 21:32:48 +0530

    ADD MEMBER DATA PAGE

--- app/src/main/java/com/example/dazzlingdreams/AddMember.java
+++ app/src/main/java/com/example/dazzlingdreams/AddMember.java
@@ -3,15 +3,32 @@ package com.example.dazzlingdreams;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
 
 public class AddMember extends AppCompatActivity {
 
+    AutoCompleteTextView autoCompleteTextView;
+    String []plans = {"Monthly","Quarterly","Half Yearly","Yearly"};
+    ArrayAdapter<String> adapteritems;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_add_member );
 
 
+        autoCompleteTextView = findViewById(R.id.auto_completetext1);
+        adapteritems = new ArrayAdapter<>( this, R.layout.list_item, plans );
+        autoCompleteTextView.setAdapter(adapteritems);
+        autoCompleteTextView.setOnItemClickListener( new AdapterView.OnItemClickListener( ) {
+            @Override
+            public void onItemClick (AdapterView<?> adapterView, View view, int position, long id) {
+                String item = adapterView.getItemAtPosition(position).toString();
+            }
+        } );
 
     }
 }
