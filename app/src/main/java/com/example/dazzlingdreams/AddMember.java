3650a35a7c37d766ad93004c704c5a5bee2814b5
Student <prafuldeshmukh068@gmail.com>
2023-04-05 13:57:05 +0530

    add member page

--- app/src/main/java/com/example/dazzlingdreams/AddMember.java
+++ app/src/main/java/com/example/dazzlingdreams/AddMember.java
@@ -10,5 +10,8 @@ public class AddMember extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_add_member );
+
+
+
     }
 }
