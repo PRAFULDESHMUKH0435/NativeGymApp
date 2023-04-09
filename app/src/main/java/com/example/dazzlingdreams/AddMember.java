f47ddd18ca38153ed66bbb445cc47138ef24776c
Praful Deshmukh <prafuldeshmukh068@gmail.com>
2023-04-09 18:30:38 +0530

    ALL UPDATED COMPONENTS LIKE CAMERA AND GALLERY , AUTOIMAGE SLIDER AND DATE LINEAR LAYOUT

--- app/src/main/java/com/example/dazzlingdreams/AddMember.java
+++ app/src/main/java/com/example/dazzlingdreams/AddMember.java
@@ -27,6 +27,7 @@ public class AddMember extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_add_member );
 
+        getSupportActionBar().setTitle("Add Member");
 
         autoCompleteTextView = findViewById(R.id.auto_completetext1);
         adapteritems = new ArrayAdapter<>( this, R.layout.list_item, plans );
