1e0ffdaf52ad451dba818de9d156c148e97ea8dd
Praful Deshmukh <prafuldeshmukh068@gmail.com>
2023-08-26 22:09:29 +0530

    UPDATED CHANGES OF NEW PROJECT

--- app/src/main/java/com/PowerZone/dazzlingdreams/AddMember.java
+++ app/src/main/java/com/PowerZone/dazzlingdreams/AddMember.java
@@ -10,6 +10,9 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dazzlingdreams.R;
 import com.github.dhaval2404.imagepicker.ImagePicker;
@@ -20,8 +23,10 @@ import java.util.Calendar;
 public class AddMember extends AppCompatActivity {
 
     AutoCompleteTextView autoCompleteTextView;
-    private Button pickDateBtn, EndDateBtn,ImgButton;
+    private Button pickDateBtn, EndDateBtn,ImgButton , RegisterUser;
     String []plans = {"Monthly","Quarterly","Half Yearly","Yearly"};
+    String plan ="", start_date="" ,end_date="";
+    boolean isloading = false;
     ArrayAdapter<String> adapteritems;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -30,13 +35,22 @@ public class AddMember extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Add Members");
 
+
+        HelperServices  helperService = new HelperServices();
+
+        ///////////
+        String username = findViewById( R.id.username).toString();
+        String email = findViewById( R.id.email).toString();
+        String mobno = findViewById( R.id.phonenumber).toString();
+        //////////
+
         autoCompleteTextView = findViewById(R.id.auto_completetext1);
         adapteritems = new ArrayAdapter<>( this, R.layout.list_item, plans );
         autoCompleteTextView.setAdapter(adapteritems);
         autoCompleteTextView.setOnItemClickListener( new AdapterView.OnItemClickListener( ) {
             @Override
             public void onItemClick (AdapterView<?> adapterView, View view, int position, long id) {
-                String item = adapterView.getItemAtPosition(position).toString();
+                 plan = adapterView.getItemAtPosition(position).toString();
             }
         } );
 
@@ -55,7 +69,7 @@ public class AddMember extends AppCompatActivity {
                             @Override
                             public void onDateSet(DatePicker view, int year,
                                                   int monthOfYear, int dayOfMonth) {
-                                String start = dayOfMonth+"-"+(monthOfYear+1)+"-"+year;
+                                 start_date = dayOfMonth+"-"+(monthOfYear+1)+"-"+year;
                                 pickDateBtn.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
                             }
                         },
@@ -80,7 +94,7 @@ public class AddMember extends AppCompatActivity {
                             @Override
                             public void onDateSet(DatePicker view, int year,
                                                   int monthOfYear, int dayOfMonth) {
-                                String end = dayOfMonth+"-"+(monthOfYear+1)+"-"+year;
+                                 end_date = dayOfMonth+"-"+(monthOfYear+1)+"-"+year;
                                 EndDateBtn.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
                             }
                         },
@@ -100,6 +114,20 @@ public class AddMember extends AppCompatActivity {
                         .compress(1024)			//Final image size will be less than 1 MB(Optional)
                         .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
                         .start();
+            }
+        } );
+
+
+        RegisterUser = findViewById(R.id.btnRegister);
+        RegisterUser.setOnClickListener( new View.OnClickListener( ) {
+            @Override
+            public void onClick (View v) {
+                if (!(username.isEmpty() || email.isEmpty() || mobno.isEmpty() || plan.isEmpty() || start_date.isEmpty() || end_date.isEmpty())){
+                        helperService.SaveDataToFireStoreDatabase("FitnessStar",username,email,mobno,plan,start_date,end_date,"65");
+                    Toast.makeText( AddMember.this, "Method Hitted Sucessfully", Toast.LENGTH_SHORT ).show( );
+                }else {
+                    Toast.makeText( AddMember.this, "All Fields Are Required", Toast.LENGTH_SHORT ).show( );
+                }
             }
         } );
 
--- app/src/main/java/com/PowerZone/dazzlingdreams/LoginPage.java
+++ app/src/main/java/com/PowerZone/dazzlingdreams/LoginPage.java
@@ -6,17 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.dazzlingdreams.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 public class LoginPage extends AppCompatActivity {
     Button createaccount,Loginbtn;
+    FirebaseUser auth;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login_page );
+
+        ///////////////////////
+        auth = FirebaseAuth.getInstance().getCurrentUser();
+
+        //////////////////////
+
 
         Objects.requireNonNull( getSupportActionBar( ) ).setTitle( "Login Page");
 
@@ -33,7 +43,7 @@ public class LoginPage extends AppCompatActivity {
         Loginbtn.setOnClickListener( new View.OnClickListener( ) {
             @Override
             public void onClick (View v) {
-                startActivity( new Intent(LoginPage.this,MainActivity.class) );
+               startActivity( new Intent(LoginPage.this,MainActivity.class) );
             }
         } );
     }
